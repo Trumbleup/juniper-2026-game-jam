ca169143eade7f12301855e8e6aaba4ae19e216d
Trumbleup <Dtrumble12@gmail.com>
2026-06-24 18:13:34 -0500

    adds mr serious, adds time for mr serious to appear when 30 seconds has passed

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,11 +1,14 @@
 extends Node2D
 
 var score: int
+var serious_time_left: int = 30
 var washerActive: bool = false
 var animals
 @export var totalPowerWeight: int = 10
 
 @export var entityHeld: bool = false
+
+@onready var mr_serious = $"Mr_ Serious"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,6 +18,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	handlePowerWeight()
+	handleSeriousAppearance()
 
 
 func _on_score_timer_timeout() -> void:
@@ -26,6 +30,12 @@ func _on_score_timer_timeout() -> void:
 func _on_washer_press(isActive: bool) -> void:
 	washerActive = isActive
 	
+func _on_serious_timer_timeout() -> void:
+	if serious_time_left == 0:
+		return
+	serious_time_left -= 1
+	$CanvasLayer/SeriousTimeLabel.text = "Mr. Serious Appears in " + str(serious_time_left) + " Seconds"
+
 func handlePowerWeight() -> void:
 	var powerWeight: int
 	for animal in animals:
@@ -33,5 +43,12 @@ func handlePowerWeight() -> void:
 			powerWeight += animal.score_weight
 	totalPowerWeight = powerWeight
 	$CanvasLayer/PowerLabel.text = str(totalPowerWeight) + "/10"
-		
+	
+func handleSeriousAppearance() -> void:
+	if serious_time_left != 0:
+		return
+	if mr_serious.visible:
+		mr_serious.visible = false
+	else:
+		mr_serious.visible = true	
 		
