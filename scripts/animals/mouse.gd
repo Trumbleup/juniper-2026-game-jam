8990b93d825082cd33f127d17de13cda3aee11eb
Trumbleup <Dtrumble12@gmail.com>
2026-06-24 17:30:00 -0500

    added another mouse and rabbit, added display for power weight

--- scripts/animals/mouse.gd
+++ scripts/animals/mouse.gd
@@ -1,5 +1,7 @@
 extends Animal
 
+@export var score_weight: int = 1
+
 const SPEED: int = 150
 
 func _process(delta: float) -> void:
--- scripts/game.gd
+++ scripts/game.gd
@@ -2,16 +2,19 @@ extends Node2D
 
 var score: int
 var washerActive: bool = false
+var animals
+var totalPowerWeight: int = 10
+
 @export var entityHeld: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
-
+	animals = get_tree().get_nodes_in_group("Animals")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	pass
+	handlePowerWeight()
 
 
 func _on_score_timer_timeout() -> void:
@@ -22,3 +25,13 @@ func _on_score_timer_timeout() -> void:
 
 func _on_washer_press(isActive: bool) -> void:
 	washerActive = isActive
+	
+func handlePowerWeight() -> void:
+	var powerWeight: int
+	for animal in animals:
+		if animal.placedOnWheel:
+			powerWeight += animal.score_weight
+	totalPowerWeight = powerWeight
+	$CanvasLayer/PowerLabel.text = str(totalPowerWeight) + "/10"
+		
+		
